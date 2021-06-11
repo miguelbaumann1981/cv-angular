42e214eb3512adbe42b1b55922a8a99070e169a6
miguelbaumann1981 <85732344+miguelbaumann1981@users.noreply.github.com>
2021-06-11 10:04:48 +0200

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// PÁGINAS
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
+import { ExperiencePageComponent } from './pages/experience-page/experience-page.component';
+import { EducationPageComponent } from './pages/education-page/education-page.component';
+import { SkillsPageComponent } from './pages/skills-page/skills-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+
+// COMPONENTES
+import { MenuComponent } from './components/menu/menu.component';
+import { ProgressSkillComponent } from './components/progress-skill/progress-skill.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    ProfilePageComponent,
+    ExperiencePageComponent,
+    EducationPageComponent,
+    SkillsPageComponent,
+    ContactPageComponent,
+    MenuComponent,
+    ProgressSkillComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
