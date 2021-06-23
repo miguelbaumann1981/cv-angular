37350827616bf20d7b6358d8266aadb9e31bd658
miguel.martinez3.contractor <miguel.martinez3.contractor@bbva.com>
2021-06-23 13:37:14 +0200

    json

--- src/app/services/data/skillsData.data.ts
+++ src/app/services/data/skillsData.data.ts
@@ -4,32 +4,7 @@ export let SKILLS_DATA: SkillsDataModel[] = [
   {
     icon: 'html.svg',
     text: 'HTML5',
-    percentage: 10,
-  },
-  {
-    icon: 'css.svg',
-    text: 'CSS3',
-    percentage: 20,
-  },
-  {
-    icon: 'js.svg',
-    text: 'JavaScript',
-    percentage: 30,
-  },
-  {
-    icon: 'boo.svg',
-    text: 'Bootstrap',
-    percentage: 40,
-  },
-  {
-    icon: 'jquery.svg',
-    text: 'jQuery',
-    percentage: 50,
-  },
-  {
-    icon: 'sass.svg',
-    text: 'Sass',
-    percentage: 60
+    percentage: 90
   },
   {
     icon: 'angular.svg',
@@ -37,25 +12,63 @@ export let SKILLS_DATA: SkillsDataModel[] = [
     percentage: 60
   },
   {
-    icon: 'xd.svg',
-    text: 'Adobe XD',
+    icon: 'css.svg',
+    text: 'CSS3',
+    percentage: 90
+  },
+  {
+    icon: 'litelement.png',
+    text: 'LitElement',
     percentage: 60
   },
   {
-    icon: 'psd.svg',
-    text: 'Adobe Photoshop',
-    percentage: 60
-  },
-  {
-    icon: 'ai.svg',
-    text: 'Adobe Illustrator',
-    percentage: 60
+    icon: 'js.svg',
+    text: 'JavaScript',
+    percentage: 50
   },
   {
     icon: 'git.svg',
     text: 'Git',
-    percentage: 60
+    percentage: 55
+  },
+  {
+    icon: 'sass.svg',
+    text: 'Sass',
+    percentage: 90
+  },
+  {
+    icon: 'polymer.svg',
+    text: 'Polymer',
+    percentage: 30
+  },
+  {
+    icon: 'boo.svg',
+    text: 'Bootstrap',
+    percentage: 80
+  },
+  {
+    icon: 'psd.svg',
+    text: 'Adobe Photoshop',
+    percentage: 80
+  },
+  {
+    icon: 'wp.svg',
+    text: 'WordPress',
+    percentage: 75
+  },
+  {
+    icon: 'xd.svg',
+    text: 'Adobe XD',
+    percentage: 80
+  },
+  {
+    icon: 'jquery.svg',
+    text: 'jQuery',
+    percentage: 50,
+  },
+  {
+    icon: 'ai.svg',
+    text: 'Adobe Illustrator',
+    percentage: 80
   }
-
-  
 ]
